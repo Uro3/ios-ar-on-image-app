86e5bd58e2725b7cb0f4dd08de9e35ae7e6c0342
Uro3 <miyakawa.uro3@gmail.com>
2019-07-16 11:18:26 +0900

    Initial Commit

---
+++ ar-on-image-appUITests/ar_on_image_appUITests.swift
@@ -0,0 +1,34 @@
+//
+//  ar_on_image_appUITests.swift
+//  ar-on-image-appUITests
+//
+//  Created by 宮川和也 on 2019/07/16.
+//  Copyright © 2019 Kazuya Miyakawa. All rights reserved.
+//
+
+import XCTest
+
+class ar_on_image_appUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
